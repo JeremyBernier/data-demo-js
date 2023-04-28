379471694d5604b6bb1822df79a8103e72aa177d
Jeremy Bernier <jeremy.s.bernier@gmail.com>
2023-04-28 16:46:02 +0700

    first

---
+++ src/components/ScatterPlot.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import {
+  Chart as ChartJS,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Scatter } from "react-chartjs-2";
+import { faker } from "@faker-js/faker";
+
+ChartJS.register(LinearScale, PointElement, LineElement, Tooltip, Legend);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top" as const,
+    },
+    title: {
+      display: true,
+      text: "Chart.js Line Chart",
+    },
+  },
+  scales: {
+    y: {
+      beginAtZero: true,
+    },
+  },
+};
+
+export const data = {
+  datasets: [
+    {
+      label: "A dataset",
+      data: Array.from({ length: 100 }, () => ({
+        x: faker.datatype.number({ min: -100, max: 100 }),
+        y: faker.datatype.number({ min: -100, max: 100 }),
+      })),
+      backgroundColor: "rgba(255, 99, 132, 1)",
+    },
+  ],
+};
+
+export default function ScatterPlot() {
+  return <Scatter options={options} data={data} />;
+}
